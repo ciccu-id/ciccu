5f266d71690c1be14fc32dce81d34f14afd34d2c
ciccu-id <ygpentinggmail22@gmail.com>
2026-09-26 23:29:07 +0700

    Update r-main.js

--- pricelist/reseller/js/r-main.js
+++ pricelist/reseller/js/r-main.js
@@ -1,13 +1,13 @@
-import{RES,resShowView}from'./r-core.js?v=5';
-import{initAuth}from'./r-auth.js?v=5';
-import{initStore}from'./r-store.js?v=5';
-import{initCheckout}from'./r-checkout.js?v=5';
-import{initOrders}from'./r-orders.js?v=5';
+import{RES,resShowView}from'./r-core.js?v=6';
+import{initAuth}from'./r-auth.js?v=6';
+import{initStore}from'./r-store.js?v=6';
+import{initCheckout}from'./r-checkout.js?v=6';
+import{initOrders}from'./r-orders.js?v=6';
 function setupNav(){
 const navStore=document.getElementById('resNavStore');
 const navOrders=document.getElementById('resNavOrders');
-if(navStore)navStore.addEventListener('click',function(){resShowView('store');});
-if(navOrders)navOrders.addEventListener('click',function(){resShowView('orders');});
+if(navStore)navStore.addEventListener('click',function(){resShowView('store')});
+if(navOrders)navOrders.addEventListener('click',function(){resShowView('orders')});
 }
 function boot(){
 initStore();
